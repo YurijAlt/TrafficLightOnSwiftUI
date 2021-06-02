51bf1b516760081b9377ea07369e29c95be7a719
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-06-03 00:58:16 +0300

    2. Finish logic

--- TrafficLightOnSwiftUI/ColorCircle.swift
+++ TrafficLightOnSwiftUI/ColorCircle.swift
@@ -9,26 +9,27 @@ import SwiftUI
 
 struct ColorCircle: View {
     
-    let color: Color // переменная для инициализации цвета
+    let color: Color
+    let opacity: Double
     
     
     var body: some View {
         Circle()
-            .foregroundColor(color)//цвет фона фигуры
-//            .ignoresSafeArea() //заливает всю площадь экрана в тч и safe area
-            .frame(width: 140, height: 140)
-            .clipShape(Circle())//обрезание по фигуре, которую передадим в иниц
-            .overlay(Circle().stroke(Color.white, lineWidth: 5))//наложение фигуры с белого цвета-srtoke-шов(граница окружности толщиной 5
-            .shadow(color: .white, radius: 8)//тень радиусом 8
+            .foregroundColor(color
+                                .opacity(opacity))
+            .frame(height: 140)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(color: .white, radius: 8)
     }
 }
 
-struct ColorCircle_Previews: PreviewProvider {//превью
+struct ColorCircle_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-            Color(.black)//фон превью
-                .ignoresSafeArea()//модификатор игнора
-            ColorCircle(color: .red)//фигура
+            Color(.black)
+                .ignoresSafeArea()
+            ColorCircle(color: .red, opacity: 1.0)
         }
     }
 }
--- TrafficLightOnSwiftUI/TrafficLightView.swift
+++ TrafficLightOnSwiftUI/TrafficLightView.swift
@@ -7,27 +7,88 @@
 
 import SwiftUI
 
-struct TrafficLightView: View {
-    var body: some View {
-        ZStack {//накалываем вьюхи
-            Color(.darkGray)//фон черного цвета
-                .ignoresSafeArea()//заливка фона в черный вместе с SafeArea
-            VStack(spacing: 30) {//вертикальный стек
-                ColorCircle(color: .red)
-                ColorCircle(color: .yellow)
-                ColorCircle(color: .green)
-                Spacer()
-            }
-           .padding(.top, 10)
-        }
-        
-
-    }
-    
+enum TrafficLightColors {
+    case red
+    case yellow
+    case green
 }
 
-struct ContentView_Previews: PreviewProvider {//превью экран справа
+struct TrafficLightView: View {
+    @State private var redColor = ColorCircle(color: .red, opacity: 0.5)
+    @State private var yellowColor = ColorCircle(color: .yellow, opacity: 0.5)
+    @State private var greenColor = ColorCircle(color: .green, opacity: 0.5)
+    
+    @State private var buttonText = "START"
+    
+    @State private var brightLight = TrafficLightColors.red
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.5
+    
+    var body: some View {
+        ZStack {
+            Color(.darkGray)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                redColor
+                yellowColor
+                greenColor
+                Spacer()
+                startButton
+            }
+            .padding(.top, 10)
+        }
+    }
+    //MARK: - Start Button Setup
+    private var startButton: some View {
+        Button(action: { changeColor() }) {
+            Text("\(buttonText)")
+        }
+        .frame(width: 140.0, height: 63.0)
+        .background(Color(.white))
+        .border(Color.white, width: 1)
+        .cornerRadius(15)
+        .foregroundColor(.black)
+        .font(.system(size: 30))
+        .padding(.bottom, 40)
+    
+}
+    //MARK: - Private Methods
+    private func changeColor() {
+        buttonText = "NEXT"
+    
+        switch brightLight {
+    
+        case .red:
+            redColor = ColorCircle(color: .red, opacity: lightIsOn)
+            greenColor = ColorCircle(color: .green, opacity: lightIsOff)
+            brightLight = .yellow
+        case .yellow:
+            redColor = ColorCircle(color: .red, opacity: lightIsOff)
+            yellowColor = ColorCircle(color: .yellow, opacity: lightIsOn)
+            brightLight = .green
+        case .green:
+            yellowColor = ColorCircle(color: .yellow, opacity: lightIsOff)
+            greenColor = ColorCircle(color: .green, opacity: lightIsOn)
+            brightLight = .red
+        }
+        
+        
+}
+
+}
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         TrafficLightView()
     }
 }
+
+
